f222403d3f0ce285cc200c48129a5f49a7e0f947
Toby Whitehead <whiteheadt99@gmail.com>
2021-09-22 10:47:34 +0100

    fixed index page

--- src/main/java/com/koby/BlackjackGame/BlackjackGameApplication.java
+++ src/main/java/com/koby/BlackjackGame/BlackjackGameApplication.java
@@ -2,12 +2,18 @@ package com.koby.BlackjackGame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class BlackjackGameApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlackjackGameApplication.class, args);
+	}
+
+	@GetMapping("/customRoute")
+	public String myResponse(){
+		return "output";
 	}
 
 }
