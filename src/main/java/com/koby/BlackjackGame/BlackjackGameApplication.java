24004b1dfa9f2f4525017eadb1c50b50c60169ef
Toby Whitehead <whiteheadt99@gmail.com>
2021-09-22 10:53:32 +0100

    fixed link to customRoute

--- src/main/java/com/koby/BlackjackGame/BlackjackGameApplication.java
+++ src/main/java/com/koby/BlackjackGame/BlackjackGameApplication.java
@@ -3,8 +3,11 @@ package com.koby.BlackjackGame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BlackjackGameApplication {
 
 	public static void main(String[] args) {
